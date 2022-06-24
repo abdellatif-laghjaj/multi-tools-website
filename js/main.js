178569fae0be2e04940625772e677f1192cf5cfc
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-24 02:33:19 +0100

    update color logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const settingsBtn = document.querySelector('.theme-icon');
 const closeSettingsBtn = document.querySelector('.close-settings-btn');
 const themePopup = document.querySelector('.theme-popup');
+const colorItems = document.querySelectorAll('.color-item');
 
 settingsBtn.addEventListener('click', showThemeSettings);
 closeSettingsBtn.addEventListener('click', hideThemeSettings);
@@ -11,5 +12,22 @@ function showThemeSettings() {
 
 function hideThemeSettings() {
     themePopup.style.display = 'none';
-    console.log('hide');
+}
+
+//color picker
+const colors = [
+    '#f44336',
+    '#e91e63',
+    '#9c27b0',
+    '#673ab7',
+    '#3f51b5',
+]
+
+for (let i = 0; i < colors.length; i++) {
+    colorItems[i].style.backgroundColor = colors[i];
+    colorItems[i].addEventListener('click', changeColor);
+}
+
+function changeColor(color) {
+    document.body.style.backgroundColor = color.target.style.backgroundColor;
 }
