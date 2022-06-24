7456e588d4b89e5dbf8d5d6bdc73cc7bb37c2d54
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-24 02:43:22 +0100

    update color theme popup

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const settingsBtn = document.querySelector('.theme-icon');
 const closeSettingsBtn = document.querySelector('.close-settings-btn');
 const themePopup = document.querySelector('.theme-popup');
 const colorItems = document.querySelectorAll('.color-item');
+const themeModeBtns = document.querySelectorAll('.theme-item');
 
 settingsBtn.addEventListener('click', showThemeSettings);
 closeSettingsBtn.addEventListener('click', hideThemeSettings);
@@ -15,7 +16,7 @@ function hideThemeSettings() {
 }
 
 //color picker
-const colors = [
+const primaryColors = [
     '#f44336',
     '#e91e63',
     '#9c27b0',
@@ -23,11 +24,33 @@ const colors = [
     '#3f51b5',
 ]
 
-for (let i = 0; i < colors.length; i++) {
-    colorItems[i].style.backgroundColor = colors[i];
-    colorItems[i].addEventListener('click', changeColor);
+const secondaryColors = [
+    '#ff9800',
+    '#ff5722',
+    '#795548',
+    '#607d8b',
+    '#3f51b5',
+];
+
+for (let i = 0; i < primaryColors.length; i++) {
+    colorItems[i].style.backgroundColor = primaryColors[i];
+    colorItems[i].addEventListener('click', () => changeColor(event, i));
 }
 
-function changeColor(color) {
-    document.body.style.backgroundColor = color.target.style.backgroundColor;
+function changeColor(color, index) {
+    document.documentElement.style.setProperty('--bg-color', color.target.style.backgroundColor);
+    document.documentElement.style.setProperty('--secondary-color', secondaryColors[index]);
 }
+
+//change theme mode
+themeModeBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if(btn.classList.contains('dark-mode')) {
+            document.documentElement.style.setProperty('--bg-color', '#212121');
+            document.documentElement.style.setProperty('--text-color', '#fff');
+        }else {
+            document.documentElement.style.setProperty('--bg-color', '#fff');
+            document.documentElement.style.setProperty('--text-color', '#000');
+        }
+    })
+});
