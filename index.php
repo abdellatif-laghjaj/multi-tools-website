0e71e35947b3720da89d855a379b990a10889f1a
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-25 01:13:11 +0100

    add data script

--- index.php
+++ index.php
@@ -28,19 +28,9 @@
     <!-- Header -->
     <section class="intro" id="intro">
 
-        <div class="blob-anim">
-            <svg id="visual" viewBox="0 0 900 600" width="900" height="600" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
-                <g transform="translate(452.3249138162694 296.1720385492431)">
-                    <path id="blob-1" d="M146.7 -136.4C190.2 -103.2 225.6 -51.6 223.3 -2.4C220.9 46.9 180.8 93.8 137.3 135.5C93.8 177.1 46.9 213.6 5.8 207.8C-35.4 202 -70.7 154 -112.5 112.4C-154.4 70.7 -202.7 35.4 -220.4 -17.7C-238 -70.7 -225.1 -141.4 -183.3 -174.6C-141.4 -207.8 -70.7 -203.4 -9.5 -193.8C51.6 -184.3 103.2 -169.6 146.7 -136.4" fill="var(--secondary-color)">
-                    </path>
-                </g>
-
-                <g transform="translate(437.86184131829555 324.6290600370678)" style="visibility: hidden;">
-                    <path id="blob-2" d="M187.9 -180.1C234.4 -141.4 256.7 -70.7 239.5 -17.2C222.3 36.3 165.6 72.6 119.1 97.6C72.6 122.6 36.3 136.3 -15.6 151.9C-67.4 167.4 -134.8 184.8 -174 159.8C-213.2 134.8 -224.1 67.4 -220.3 3.8C-216.5 -59.9 -198.1 -119.7 -158.9 -158.4C-119.7 -197.1 -59.9 -214.5 5.4 -220C70.7 -225.4 141.4 -218.8 187.9 -180.1" fill="var(--secondary-color)">
-                    </path>
-                </g>
-            </svg>
-        </div>
+        <!-- Blob -->
+        <?php include 'components/blob.html'; ?>
+        
         <header>
             <a href="" class="logo">Tooly<span>.</span></a>
             <nav>
@@ -112,6 +102,7 @@
 
     <!-- JS -->
     <script src="js/theme_script.js"></script>
+    <script src="js/data.js"></script>
     <script src="js/main.js"></script>
     <script>
         feather.replace();
---
+++ js/data.js
@@ -0,0 +1,12 @@
+const PROJECTS_DATA = [
+    {
+        id: 1,
+        title: 'Project 1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        link: '#',
+        github: '#',
+        tech: ['HTML', 'CSS', 'JS'],
+        isPopular: true,
+    },
+];
