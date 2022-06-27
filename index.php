1f182c77ae54f6170412e525a521823eb6d29d62
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-27 21:15:10 +0100

    make intro section responsive

--- index.php
+++ index.php
@@ -33,8 +33,8 @@
         
         <header>
             <a href="" class="logo">Abdel<span>.</span></a>
-            <nav class="active">
-                <ul class="active">
+            <nav>
+                <ul>
                     <li class="nav-link active-link">
                         <a href="#">Home</a>
                     </li>
