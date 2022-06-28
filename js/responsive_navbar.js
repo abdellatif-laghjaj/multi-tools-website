3943e0aacf09e810be96b8fd9baafe27e17c8dcc
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-28 02:34:11 +0100

    update swiper

--- js/responsive_navbar.js
+++ js/responsive_navbar.js
@@ -1,3 +1,7 @@
 const header = document.querySelector('header');
 const nav = document.querySelector('nav');
 const menuButton = document.querySelector('.menu-btn');
+
+function shwoSidebar() {
+  nav.classList.toggle('mobile-nav');
+}
