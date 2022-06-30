9f946de1a25e29ea68351c078a92ee0c77af328a
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-30 03:23:34 +0100

    update nav for mobile

--- js/responsive_navbar.js
+++ js/responsive_navbar.js
@@ -1,9 +1,21 @@
 const navBar = document.querySelector('nav'),
-  menuButton = document.querySelector('.menu-icon-fixed');
+  menuButton = document.querySelector('.menu-icon-fixed'),
+  menuIcon = document.querySelector('.menu-icon');
 
 
 menuButton.addEventListener('click', () => {
   navBar.classList.toggle('mobile-nav');
   //add class to elements inside navbar
   navBar.querySelector('ul').classList.toggle('mobile-nav');
+  //change data-feather attribute of menu-icon from grid to x
+  menuButton.setAttribute('data-feather', 'x');
 });
+
+navBar.querySelectorAll('ul li').forEach(li => {
+  li.addEventListener('click', () => {
+    navBar.classList.remove('mobile-nav');
+    navBar.querySelector('ul').classList.remove('mobile-nav');
+    menuButton.setAttribute('data-feather', 'grid');
+  });
+}
+);
