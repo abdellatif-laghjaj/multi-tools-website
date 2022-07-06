d842f87688ffe515c6f0e7012df7caa3df5bd2bb
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-06 18:28:07 +0100

    more animations

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,9 @@
-const intro_text = document.querySelector('.intro-text');
-
+//Aimations for intro
 ScrollReveal().reveal('.intro-text', {
     duration: 2000,
     origin: 'left',
     distance: '300px',
-    delay: 100,
+    delay: 300,
     easing: 'ease-in-out'
 });
 
@@ -12,6 +11,84 @@ ScrollReveal().reveal('.swiper', {
     duration: 2000,
     origin: 'right',
     distance: '300px',
-    delay: 200,
+    delay: 300,
+    easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal('h3', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    delay: 300,
+    easing: 'ease-in-out'
+});
+
+//Aimations for about
+ScrollReveal().reveal('.about-text', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    delay: 300,
+    easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal('.about-image', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px',
+    delay: 400,
+    easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal('.about-skills', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px',
+    delay: 300,
+    easing: 'ease-in-out'
+});
+
+//Aimations for services
+ScrollReveal().reveal('.service-card', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px',
+    delay: 300,
+    easing: 'ease-in-out'
+});
+
+//Aimations for projects
+ScrollReveal().reveal('#projects p', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    delay: 300,
+    easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal('#projects .container .card', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px',
+    delay: 300,
+    easing: 'ease-in-out'
+});
+
+//Aimations for contact
+for (let i = 0; i < 4; i++) {
+    ScrollReveal().reveal(`#contact-form .input-container.icp${i+1}`, {
+        duration: 2000,
+        origin: 'left',
+        distance: '300px',
+        delay: (1 + i) * 100,
+        easing: 'ease-in-out'
+    });
+}
+
+ScrollReveal().reveal('.submit-btn', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px',
+    delay: 500,
     easing: 'ease-in-out'
 });
